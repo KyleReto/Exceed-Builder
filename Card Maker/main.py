972fda55b580cefa3103bd8487c6f9cbea8ab4c4
KyleReto <40499747+KyleReto@users.noreply.github.com>
2022-02-17 18:45:24 -0500

    Added more structural elements to the GUI

--- Card Maker/main.py
+++ Card Maker/main.py
@@ -3,30 +3,63 @@ from tkinter import ttk
 from PIL import ImageTk,Image
 import glob
 
-# Outline the basic structure
+# Sorry for this, but I have no idea how to create a GUI properly, so I'm fumbling around here.
+# If the code seems disorganized, that's because it is.
+
+def collapse(frame, col_index):
+    Grid.columnconfigure(frame, col_index, weight=0)
+
+def expand(frame, col_index):
+    Grid.columnconfigure(frame, col_index, weight=1)
+
+# Create and set up the root window.
 root = Tk()
 root.title("Exceed Builder")
+root.geometry("852x480")
+
+# Set up the grid
 Grid.columnconfigure(root, 0, weight=1)
 Grid.rowconfigure(root, 0, weight=1)
 
+# Set up the main frame, which is the frame that contains everything else.
 mainframe = ttk.Frame(root)
 mainframe.grid(column=0, row=0, sticky=N+E+S+W)
 
-# TODO: Clean this up
-for row_index in range(3):
-    Grid.rowconfigure(mainframe, row_index, weight=1)
-    for col_index in range(3):
-        Grid.columnconfigure(mainframe, col_index, weight=1)
+# Set up main grid
+Grid.rowconfigure(mainframe, 0)
+Grid.rowconfigure(mainframe, 1, weight=1)
+for col_index in range(3):
+    Grid.columnconfigure(mainframe, col_index, weight=1)
+Grid.columnconfigure(mainframe, 1, weight=2)
 
+# Set up all the styles we'll be using.
 style = ttk.Style()
-style.configure("blackbox.TFrame", foreground='black', background='black')
+style.configure("elements.TFrame", background='grey')
+style.configure("stats.TFrame", background='grey')
+style.configure("menu_bar.TFrame", background='grey')
+style.configure("menu.TButton")
 
-elements_frame = ttk.Frame(mainframe, style='blackbox.TFrame').grid(column=0, row=0, sticky=N+E+S+W)
-canvas_frame = ttk.Frame(mainframe).grid(column=1, row=0, sticky=N+E+S+W)
-stats_frame = ttk.Frame(mainframe).grid(column=2, row=0, sticky=N+E+S+W)
+# Set up frames for each segment of the GUI
+menu_bar = ttk.Frame(mainframe, style='menu_bar.TFrame')
+menu_bar.grid(column=0, row=0, columnspan=3, sticky=NW)
+for col_index in range(5):
+    Grid.columnconfigure(menu_bar, col_index)
 
+elements_frame = ttk.Frame(mainframe, style='elements.TFrame')
+elements_frame.grid(column=0, row=1, rowspan=3, sticky=N+E+S+W)
 
-ttk.Label(elements_frame, text="test")
+canvas_frame = ttk.Frame(mainframe)
+canvas_frame.grid(column=1, row=1, rowspan=3, sticky=N+E+S+W)
+
+stats_frame = ttk.Frame(mainframe, style="stats.TFrame")
+stats_frame.grid(column=2, row=1, rowspan=3, sticky=N+E+S+W)
+
+# Set up the menu bar, which allows the user to complete various application-level actions, like saving or loading.
+menu_bar_button_padding = "15 4 10 4"
+ttk.Button(menu_bar, text="File", width=4, style="Toolbutton", padding=menu_bar_button_padding).grid(column=0, row=0, sticky=NW)
+ttk.Button(menu_bar, text="Edit", width=4, style="Toolbutton", padding=menu_bar_button_padding).grid(column=1, row=0, sticky=NW)
+
+#ttk.Label(elements_frame, text="test").grid(column=1, row=0, sticky=N)
 
 # img = ImageTk.PhotoImage(Image.open("Templates\Default\Stat Blocks\Armor.png"))
 
