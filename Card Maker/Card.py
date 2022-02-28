b1229360a26720652f29e84791b1f2db7f825974
KyleReto <40499747+KyleReto@users.noreply.github.com>
2022-02-28 17:41:11 -0500

    Reworked default and optional values

--- Card Maker/Card.py
+++ Card Maker/Card.py
@@ -5,35 +5,41 @@ class Card:
     def __init__(self):
         self.card_type = ''
         self.attack_name = ''
+        self.attack_cost = ''
+        self.effects = ''
         self.range = ''
         self.power = ''
         self.speed = ''
+        self.guard = ''
+        self.armor = ''
         self.boost_name = ''
+        self.boost_cost = ''
         self.boost_effect = ''
-
-    def is_valid(self):
-        for prop in self.__dict__:
-            if self.__getattribute__(prop) == '':
-                return False
-        return True
+    
+    def property_to_str_if_present(self, property_name, proper_name):
+        if hasattr(self, property_name):
+            return f",\n   \"{proper_name}\":\"{self.__getattribute__(property_name)}\""
+        else:
+            return ""
 
     def to_json(self):
-
-        if not self.is_valid():
-            raise RuntimeError("Card is missing one or more default properties")
 
         # Add all mandatory elements
         output = ("{\n"
         f"   \"Card Type\":\"{self.card_type}\",\n"
         f"   \"Attack Name\":\"{self.attack_name}\",\n"
+        f"   \"Attack Cost\":\"{self.attack_cost}\",\n"
+        f"   \"Effects\":\"{self.effects}\",\n"
         f"   \"Range\":\"{self.range}\",\n"
         f"   \"Power\":\"{self.power}\",\n"
         f"   \"Speed\":\"{self.speed}\",\n"
+        f"   \"Guard\":\"{self.guard}\",\n"
+        f"   \"Armor\":\"{self.armor}\",\n"
         f"   \"Boost Name\":\"{self.boost_name}\",\n"
+        f"   \"Boost Cost\":\"{self.boost_cost}\",\n"
         f"   \"Boost Effect\":\"{self.boost_effect}\"")
 
-        # Add all optional elements, if present
-        # TODO: this lol
+        # TODO: Add all optional elements, if present
 
         # Close the object and return it
         output += "\n}"
--- Card Maker/main.py
+++ Card Maker/main.py
@@ -16,25 +16,38 @@ def input_required(parameter_name):
             return user_input
         print('This field is required.')
 
+def input_optional(parameter_name):
+    return input(parameter_name + ' [None]: ')
 
 action_type = 0
 while (action_type != 3):
-    action_type = int(input("""Select an option:
-    1. Create a New Card
-    2. Edit an Existing Card
-    3. Exit\n"""))
+    try:
+        action_type = int(input("""Select an option:
+        1. Create a New Card
+        2. Edit an Existing Card
+        3. Exit\n"""))
+    except ValueError:
+        pass
     card = Card()
 
     if action_type == 1:
+        print("Set the card's stats. Press enter to use the default value, denoted in [brackets] where present.")
         card.card_type = input_required("Card Type (Normal, Special, or Ultra)")
         card.attack_name = input_required("Attack Name")
+        print("Costs are written as #F or #G, for Force or Gauge costs. Ex. '4G' is 4 Gauge, while 1F is 1 Force.")
+        card.attack_cost = input_optional("Attack Cost")
+        card.effects = input_optional("Attack Effects")
+        print("Range is written as it would be on the card. Ex. '1', '2-3', 'X', and 'N/A' are all valid.")
         card.range = input_required("Attack Range")
         card.power = input_required("Attack Power")
         card.speed = input_required("Attack Speed")
+        card.guard = input_optional("Attack Guard")
+        card.armor = input_optional("Attack Armor")
         card.boost_name = input_required("Boost Name")
+        print("Boost costs are written the same as attack costs, as #F or #G.")
+        card.boost_cost = input_optional("Boost Cost")
         card.boost_effect = input_required("Boost Effect")
-        print("The following parameters are optional. Press [Enter] to skip any.")
-        # TODO: Add all optional parameters
+        
         # TODO: Add file output
         # TODO: Add export functionality
         print(card.to_json())
